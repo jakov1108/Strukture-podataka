78619d879184667761cb4e6be6fabd597671e58c
LLeski00 <115641434+LLeski00@users.noreply.github.com>
2023-01-25 22:17:23 +0100

    Update obj.cpp

--- obj.cpp
+++ obj.cpp
@@ -13,47 +13,63 @@
 #include <QPainter>
 #include <QFileDialog>
 
+int minimalno(int a, int b, int c, int d){
+    return std::min( std::min(a,b), std::min(c,d) );
+}
+
 class MyMainWindow : public QMainWindow {
     public:
     MyMainWindow();
     QLabel* MyLabel;
     void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
     QMenu* EditMenu;
     QAction* EditNoviTekst;
     void EditNoviTekstMenu();
-    void keyPressEvent(QKeyEvent *event) override;
     void paintEvent(QPaintEvent *event) override;
     void FileSaveAsMenu();
     void FileOpenMenu();
     QMenu* FileMenu;
     QAction* FileSaveAs;
     QAction* FileOpen;
+    bool mousePress;
+    QPoint start;
+    QPoint end;
+    QRect rect;
 };
 
-void MyMainWindow::paintEvent(QPaintEvent*){
-    QSize size = this->size();
+void MyMainWindow::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
-    painter.drawLine(0, size.height(), size.width(), 5);
+    if(mousePress)
+        painter.setPen(QPen(Qt::black, 2, Qt::DashLine));
+    else
+        painter.setPen(QPen(Qt::black, 2));
+    painter.drawRect(rect);
 }
-
 
 void MyMainWindow::keyPressEvent(QKeyEvent *event) {
     switch( event->key() ){
-        case Qt::Key_Left: MyLabel->move(MyLabel->pos().x()-1,
-            MyLabel->pos().y());
+        case Qt::Key_Left:
+            MyLabel->move(MyLabel->x()-1,MyLabel->y());
+            rect.moveTo(rect.x()-1,rect.y());
             break;
-        case Qt::Key_Right: MyLabel->move(MyLabel->pos().x()+1,
-            MyLabel->pos().y());
+        case Qt::Key_Right:
+            MyLabel->move(MyLabel->x()+1,MyLabel->y());
+            rect.moveTo(rect.x()+1,rect.y());
             break;
-        case Qt::Key_Up: MyLabel->move(MyLabel->pos().x(),
-            MyLabel->pos().y()-1);
+        case Qt::Key_Up:
+            MyLabel->move(MyLabel->x(),MyLabel->y()-1);
+            rect.moveTo(rect.x(),rect.y()-1);
             break;
-        case Qt::Key_Down: MyLabel->move(MyLabel->pos().x(),
-            MyLabel->pos().y()+1);
+        case Qt::Key_Down:
+            MyLabel->move(MyLabel->x(),MyLabel->y()+1);
+            rect.moveTo(rect.x(),rect.y()+1);
             break;
     }
+    update();
 }
-
 
 class MyDialog : public QDialog {
     public:
@@ -87,86 +103,102 @@ void MyMainWindow::FileSaveAsMenu(){
     if (!fileName.isEmpty()) {
         QFile file(fileName);
         if (!file.open(QIODevice::WriteOnly)) {
-            QMessageBox::information(this, "Unable to Open File", file.errorString());
-    return;
-    }
-    QTextStream out(&file);
-    out << "fesb file" << Qt::endl;
-    out << MyLabel->text() << Qt::endl;
-    out << MyLabel->pos().x() << Qt::endl;
-    out << MyLabel->pos().y() << Qt::endl;
-    out << pos().x() << Qt::endl;
-    out << pos().y() << Qt::endl;
-    out << size().width() << Qt::endl;
-    out << size().height() << Qt::endl;
+            QMessageBox::critical(this, "Error", "Could not open file for writing");
+            return;
+        }
+        QTextStream out(&file);
+        out << MyLabel->text() << Qt::endl;
+        out << MyLabel->geometry().x() << Qt::endl;
+        out << MyLabel->geometry().y() << Qt::endl;
+        out << MyLabel->geometry().width() << Qt::endl;
+        out << MyLabel->geometry().height() << Qt::endl;
+        out << rect.x() << Qt::endl;
+        out << rect.y() << Qt::endl;
+        out << rect.width() << Qt::endl;
+        out << rect.height() << Qt::endl;
+        file.close();
     }
 }
 
 void MyMainWindow::FileOpenMenu(){
-    QString fileName = QFileDialog::getOpenFileName(this, "Open Geometry...", "", "FESB File (*.fsb)");
+    QString fileName = QFileDialog::getOpenFileName(this,"Open...", "", "FESB File (*.fsb)");
     if (!fileName.isEmpty()) {
         QFile file(fileName);
         if (!file.open(QIODevice::ReadOnly)) {
-            QMessageBox::information(this, "Unable to Open File", file.errorString());
+            QMessageBox::critical(this, "Error", "Could not open file for reading");
             return;
         }
         QTextStream in(&file);
-        QString str; str = in.readLine();
-        if(str=="fesb file") {
-            str = in.readLine();
-            MyLabel->setText(str);
-            int x, y, w, h;
-            in >> x >> y;
-            MyLabel->move(x, y);
-            in >> x >> y >> w >> h;
-            this->setGeometry(x, y, w, h);
-        }
+        QString text = in.readLine();
+        int x = in.readLine().toInt();
+        int y = in.readLine().toInt();
+        int labelWidth = in.readLine().toInt();
+        int labelHeight = in.readLine().toInt();
+        int rx = in.readLine().toInt();
+        int ry = in.readLine().toInt();
+        int width = in.readLine().toInt();
+        int height = in.readLine().toInt();
+        MyLabel->setText(text);
+        MyLabel->setGeometry(x+3, y, labelWidth, labelHeight);
+        rect.setX(rx);
+        rect.setY(ry);
+        rect.setWidth(width);
+        rect.setHeight(height);
+        update();
+        file.close();
     }
 }
 
-MyMainWindow::MyMainWindow() {
-    MyLabel = new QLabel(this);
-    MyLabel->setText("Hello World!");
-    MyLabel->move(10, 20);
-    EditNoviTekst = new QAction(tr("&Novi tekst..."), this);
-    EditNoviTekst->setShortcut(tr("CTRL+N"));
-    connect(EditNoviTekst, &QAction::triggered, this,
-    &MyMainWindow::EditNoviTekstMenu);
-    EditMenu = menuBar()->addMenu(tr("&Edit"));
-    EditMenu->addAction(EditNoviTekst);
-    FileSaveAs = new QAction(tr("&Save As..."), this);
-    FileSaveAs->setShortcut(tr("CTRL+S"));
-    connect(FileSaveAs, &QAction::triggered, this,
-    &MyMainWindow::FileSaveAsMenu);
-    FileOpen = new QAction(tr("&Open..."), this);
-    FileOpen->setShortcut(tr("CTRL+O"));
-    connect(FileOpen, &QAction::triggered, this,
-    &MyMainWindow::FileOpenMenu);
-    FileMenu = menuBar()->addMenu(tr("&File"));
+MyMainWindow::MyMainWindow(){
+    mousePress = false;
+    MyLabel = new QLabel("Hello World!", this);
+    MyLabel->move(30,30);
+    EditMenu = menuBar()->addMenu("Edit");
+    EditNoviTekst = EditMenu->addAction("Edit Text");
+    connect(EditNoviTekst, &QAction::triggered, this, &MyMainWindow::EditNoviTekstMenu);
+    FileSaveAs = new QAction("Save As...", this);
+    connect(FileSaveAs, &QAction::triggered, this, &MyMainWindow::FileSaveAsMenu);
+    FileOpen = new QAction("Open...", this);
+    connect(FileOpen, &QAction::triggered, this, &MyMainWindow::FileOpenMenu);
+    FileMenu = menuBar()->addMenu("File");
     FileMenu->addAction(FileSaveAs);
     FileMenu->addAction(FileOpen);
 }
 
-void MyMainWindow::EditNoviTekstMenu() {
+void MyMainWindow::EditNoviTekstMenu(){
     MyDialog dialog;
-    int ret = dialog.exec();
-    if( ret == QDialog::Accepted ) {
+    if(dialog.exec() == QDialog::Accepted) {
         MyLabel->setText(dialog.lineEdit->text());
     }
 }
 
-void MyMainWindow::mousePressEvent(QMouseEvent* event) {
-    if (event->button() == Qt::LeftButton) {
-        MyLabel->move(int(event->position().x()),
-        int(event->position().y()));
+void MyMainWindow::mouseMoveEvent(QMouseEvent *event){
+    if(mousePress){
+        end = event->pos();
+        rect = QRect(start.x(), start.y(), end.x()-start.x(), end.y()-start.y());
+        update();
     }
 }
 
+void MyMainWindow::mouseReleaseEvent(QMouseEvent *event){
+    if(event->button() == Qt::LeftButton){
+        mousePress = false;
+        MyLabel->setGeometry(minimalno(rect.topLeft().x(),rect.bottomLeft().x(),rect.topRight().x(),rect.bottomRight().x())+3,std::min(rect.topLeft().y(),rect.bottomLeft().y()), MyLabel->size().width(), MyLabel->size().height());
+        update();
+    }
+}
+
+void MyMainWindow::mousePressEvent(QMouseEvent *event){
+    if(event->button() == Qt::LeftButton){
+        mousePress = true;
+        start = event->pos();
+    }
+}
 
 int main(int argc, char **argv) {
     QApplication app (argc, argv);
     MyMainWindow mainWindow;
-    mainWindow.resize(300,150);
+    mainWindow.resize(600,300);
     mainWindow.show();
     return app.exec();
 }
