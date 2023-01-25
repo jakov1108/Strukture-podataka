ef20953f75895fabf460bd3c30484a10206a865b
jakov1108 <115407509+jakov1108@users.noreply.github.com>
2023-01-25 22:23:08 +0100

    Update kvizAdminHeader.h
    Razdvojen header file u dva

--- projekt/kvizAdminHeader.h
+++ projekt/kvizAdminHeader.h
@@ -1,41 +1,5 @@
-#ifndef ADMIN_HEAD
-#define ADMIN_HEAD
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-#define SUCCESS (0)
-#define PROGRAM_EXIT (-1)
-#define ALLOC_FAIL_POZ (NULL)
-#define MAX_LEN (150)
-#define TEXT (100)
-#define EXIT (0)
-#define EMPTY (0)
-#define ERROR (-1)
-#define FILE_FAIL (-1)
-
-typedef struct node* position;
-struct node {
-    char question[MAX_LEN];
-    char answer[MAX_LEN];
-    position left;
-    position right;
-};
-typedef struct node node;
-
-typedef struct player* player_position;
-struct player {
-    char name[MAX_LEN];
-    double time;
-    int numCorrectAnswers;
-    //int numQuestions;
-    double averageAnswer;
-    player_position next;
-};
-typedef struct player player;
+#ifndef ADMIN_FUNCTIONS
+#define ADMIN_FUNCTIONS
 
 position createNode(position);
 position insertNode(position, position);
@@ -49,8 +13,5 @@ void printInstructions();
 position deleteQuestion(position, char*);
 position findMinNode(position);
 void deleteTree(position);
-
-int count = 0;
-int status = 0;
 
 #endif
